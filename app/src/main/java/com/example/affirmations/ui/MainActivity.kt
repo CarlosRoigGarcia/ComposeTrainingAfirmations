399654984eda88faf0125813fe14a654ccfc015f
aaa <a.a@a.a>
2023-03-22 19:58:54 +0100

    DoingProves

--- app/src/main/java/com/example/affirmations/ui/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/ui/MainActivity.kt
@@ -15,54 +15,14 @@
  */
 package com.example.affirmations.ui
 
-import android.content.ActivityNotFoundException
-import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material.icons.filled.Phone
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.affirmations.data.Datasource
-import com.example.affirmations.model.Affirmation
-import com.example.affirmations.ui.theme.AffirmationsTheme
-import com.example.affirmations.ui.viewmodel.DetailViewModel
 import com.example.affirmations.ui.viewmodel.MainViewModel
-
-
-/*private const val Tag = "main act"
-private const val TagResume = "resume act"
-private const val TagRestart = "restart act"
-private const val TagPause = "pause act"
-private const val TagDestroy = "destroy act"
-private const val TagStop = "stop act"
-private const val TagStart = "start act"*/
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
